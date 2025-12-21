56215d02e905f9e7270c077b48390c40e249e67d
Hamed Moshtagh <hamedmoshtagh0000@gmail.com>
2025-12-21 16:39:51 +0330

    Create Crud Operation in app
    create add option & update & Delete and task complete

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView,CreateTaskView,DeleteTaskView,UpdateTaskView,TaskCompleteView
 
 urlpatterns = [
-    path('',HomePageView.as_view(),name='home')
+    path('',HomePageView.as_view(),name='home'),
+    path('create/',CreateTaskView.as_view(),name='create'),
+    path('delete/<int:pk>',DeleteTaskView.as_view(),name='delete'),
+    path('update/<int:pk>',UpdateTaskView.as_view(),name='update'),
+    path("status/<int:pk>/", TaskCompleteView.as_view(), name="status"),
 ]
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView,DeleteView,UpdateView
 from .models import Task
+from django.urls import reverse_lazy
+from django.shortcuts import redirect,get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 # Create your views here.
 
 
@@ -8,3 +12,38 @@ class HomePageView(ListView):
     model = Task
     template_name = 'index.html'
     context_object_name = 'tasks'
+    
+    
+class CreateTaskView(CreateView):
+    model = Task
+    fields = ["task"]
+    template_name = "index.html"
+    success_url = reverse_lazy("home")
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class DeleteTaskView(DeleteView):
+    model = Task
+    success_url = reverse_lazy("home")
+    def get(self, request, pk, *args, **kwargs):
+        task = get_object_or_404(Task, pk=pk)
+        task.delete()
+        return redirect('home')
+    
+    
+class UpdateTaskView(UpdateView):
+    model = Task
+    fields = ['task']
+    success_url = reverse_lazy("home")
+    template_name = "update.html"
+
+
+class TaskCompleteView(View):
+    model = Task
+    def get(self, request, pk, *args, **kwargs):
+        task = get_object_or_404(Task, pk=pk)
+        task.status = not task.status  # تغییر وضعیت
+        task.save()
+        return redirect('home')
