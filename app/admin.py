b4b86a85304e3cecdf2ee566b11a20d6f003a1d7
Hamed Moshtagh <hamedmoshtagh0000@gmail.com>
2025-12-21 15:26:23 +0330

    Create Models and view

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['user','task','created_date','updated_date','status']
+    list_display_links = ['user']
+    list_filter = ['created_date']
+    ordering = ('-created_date',)
+
+
+admin.site.register(Task,TaskAdmin)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Task
 # Create your views here.
+
+
+class HomePageView(ListView):
+    model = Task
+    template_name = 'index.html'
+    context_object_name = 'tasks'
